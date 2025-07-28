bd80f023669007153dd8e40347b28f0c8d1cfc83
KIHLYouns <kihlyounes123@gmail.com>
2025-07-28 15:48:11 +0100

    feat: ajouter des filtres pour la gestion des demandes de congé en équipe

---
+++ force-app/main/default/lwc/teamLeaveManagement/teamLeaveManagement.js
@@ -0,0 +1,19 @@
+import { LightningElement, wire } from 'lwc';
+import { publish, MessageContext } from 'lightning/messageService';
+import LEAVE_DATA_FOR_CALENDAR_CHANNEL from '@salesforce/messageChannel/LeaveDataForCalendarChannel__c';
+
+export default class TeamLeaveManagement extends LightningElement {
+    activetabContent = 'team';
+
+    @wire(MessageContext)
+    messageContext;
+
+    tabChangeHandler(event) {
+        this.activetabContent = event.target.value;
+
+        const payload = {
+            context: this.activetabContent
+        };
+        publish(this.messageContext, LEAVE_DATA_FOR_CALENDAR_CHANNEL, payload);
+    }
+}
